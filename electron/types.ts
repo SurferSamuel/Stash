6523a9a925622a6cffb51bfc5e059fcd08112c52
Samuel Johnston <sbj04@outlook.com>
2024-05-11 23:22:34 +1000

    Restructured companyData type to include share history

--- electron/types.ts
+++ electron/types.ts
@@ -1,8 +1,10 @@
 import { Dayjs } from "dayjs";
 import { Quote } from "yahoo-finance2/dist/esm/src/modules/quote";
 
+// Fetched quote return type from yahoo-finance2
 export type FetchQuote = { quote: Quote };
 
+// Valid option keys, used for company details and users
 export type OptionKey =
   | "financialStatus"
   | "miningStatus"
@@ -12,12 +14,15 @@ export type OptionKey =
   | "resources"
   | "users";
 
-export type Key = OptionKey | "countries" | "data" | "settings";
+// All valid keys
+export type Key = OptionKey | "countries" | "companies" | "settings";
 
+// Dropdown option type
 export interface Option {
   label: string;
 }
 
+// Dropdown country type
 export interface Country {
   label: string;
   code: string;
@@ -25,56 +30,80 @@ export interface Country {
   suggested?: boolean;
 }
 
+// Written note type
 export interface Note {
   title: string;
   date: Dayjs;
   description: string;
 }
 
+// Date notification type
 export interface DateNotification {
   title: string;
   date: Dayjs;
 }
 
+// Price notification type
 export interface PriceNotification {
   title: string;
   highPrice: string;
   lowPrice: string;
 }
 
-export interface Share {
+// Share entry type
+export interface ShareEntry {
   user: string;
   date: Dayjs;
   quantity: string;
-  unitCost: string;
+  unitPrice: string;
   brokerage: string;
   gst: string;
   total: string;
 }
 
-export interface MainData {
+// Transaction entry type
+export interface TransactionEntry {
+  type: "BUY" | "SELL";
+  user: string;
+  date: Dayjs;
+  quantity: string;
+  unitPrice: string;
+  brokerage: string;
+  gst: string;
+  total: string;
+}
+
+// All data for a given company
+export interface CompanyData {
   asxcode: string;
-  operatingCountries: Country[];
-  financialStatus: Option[];
-  miningStatus: Option[];
-  resources: Option[];
-  products: Option[];
-  recommendations: Option[];
-  monitor: Option[];
-  reasonsToBuy: string;
-  reasonsNotToBuy: string;
-  positives: string;
-  negatives: string;
+  details: {
+    operatingCountries: Country[];
+    financialStatus: Option[];
+    miningStatus: Option[];
+    resources: Option[];
+    products: Option[];
+    recommendations: Option[];
+    monitor: Option[];
+    reasonsToBuy: string;
+    reasonsNotToBuy: string;
+    positives: string;
+    negatives: string;
+  };
   notes: Note[];
   dateNotifications: DateNotification[];
   priceNotifications: PriceNotification[];
-  shares: Share[];
+  shares: {
+    current: ShareEntry[];
+    history: TransactionEntry[];
+  };
 }
 
+// Settings type
 export interface Settings {
-  unitCostAutoFill: boolean;
+  unitPriceAutoFill: boolean;
   gstPercent: string;
   brokerageAutoFill: string;
 }
 
-export type Data = Option[] | Country[] | MainData[] | Settings;
+// Any data type, returned from getData
+export type Data = Option[] | Country[] | CompanyData[] | Settings;
