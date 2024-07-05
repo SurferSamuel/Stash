c72cd3749004dfa952d71140ec353b7197528f96
Samuel Johnston <sbj04@outlook.com>
2024-07-05 22:43:09 +1000

    Logs are now stored in a folder

--- electron/api/logs.ts
+++ electron/api/logs.ts
@@ -3,10 +3,39 @@ import dayjs from "dayjs";
 import path from "path";
 import fs from "fs";
 
+// Ensure log folder exists
+const dirPath = path.join(storage.getDataPath(), "logs");
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+// Rename existing 'latest.log' (if exists)
+const latestPath = path.join(dirPath, "latest.log");
+if (fs.existsSync(latestPath)) {
+  // Get the date of when 'latest.log' was made
+  const dateCreated = fs.statSync(latestPath).birthtime;
+  const dateString = dayjs(dateCreated).format("DD-MM-YYYY");
+
+  // Helper function that returns the path name given the number
+  // Eg. 05-07-2024_01
+  const newPath = (num: number) => {
+    return path.join(dirPath, `${dateString}_${String(num).padStart(2, '0')}.log`);
+  }
+  
+  // Find the next avaliable number
+  let i = 1;
+  while (fs.existsSync(newPath(i))) {
+    i++;
+  }
+
+  // Rename the latest log
+  fs.renameSync(latestPath, newPath(i));
+}
+
 // Setup the log file
-const filePath = path.join(storage.getDataPath(), "latest.log");
-fs.writeFileSync(filePath, "");
-const stream = fs.createWriteStream(filePath, { flags: "a" });
+const logPath = path.join(dirPath, "latest.log");
+fs.writeFileSync(logPath, "");
+const stream = fs.createWriteStream(logPath, { flags: "a" });
 
 /*
  * Appends the given message to the log file
