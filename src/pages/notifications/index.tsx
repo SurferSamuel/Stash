6aebcb74edc342a8499a133ca8a1e58037d48ed1
Samuel Johnston <sbj04@outlook.com>
2024-09-21 17:28:25 +1000

    Added notifications page

---
+++ src/pages/notifications/index.tsx
@@ -0,0 +1,18 @@
+// Material UI
+import Box from "@mui/material/Box";
+
+// Components
+import Header from "../../components/header";
+
+const Notifications = () => {
+  return (
+    <Box m="25px 30px 15px 30px">
+      <Header
+        title="Notifications"
+        subtitle="TODO"
+      />
+    </Box>
+  );
+}
+
+export default Notifications;
