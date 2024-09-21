24c3956fb3e84633ca75b49b58df493bfa31b894
Samuel Johnston <sbj04@outlook.com>
2024-09-21 17:28:05 +1000

    Added accounts page

---
+++ src/pages/accounts/index.tsx
@@ -0,0 +1,18 @@
+// Material UI
+import Box from "@mui/material/Box";
+
+// Components
+import Header from "../../components/header";
+
+const Accounts = () => {
+  return (
+    <Box m="25px 30px 15px 30px">
+      <Header
+        title="Accounts"
+        subtitle="TODO"
+      />
+    </Box>
+  );
+}
+
+export default Accounts;
