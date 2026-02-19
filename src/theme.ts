f6ec956a5460eba1d87305379cf57629ea19f254
Samuel Johnston <sbj04@outlook.com>
2026-02-20 00:02:57 +1100

    Migrated non.geist to v2

--- src/theme.ts
+++ src/theme.ts
@@ -138,30 +138,30 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: 'Geist Variable, Arial, sans-serif',
+    fontFamily: 'Geist-Variable, Arial, sans-serif',
     fontSize: 12,
     h1: {
-      fontFamily: 'Geist Variable, Arial, sans-serif',
+      fontFamily: 'Geist-Variable, Arial, sans-serif',
       fontSize: 40,
     },
     h2: {
-      fontFamily: 'Geist Variable, Arial, sans-serif',
+      fontFamily: 'Geist-Variable, Arial, sans-serif',
       fontSize: 32,
     },
     h3: {
-      fontFamily: 'Geist Variable, Arial, sans-serif',
+      fontFamily: 'Geist-Variable, Arial, sans-serif',
       fontSize: 24,
     },
     h4: {
-      fontFamily: 'Geist Variable, Arial, sans-serif',
+      fontFamily: 'Geist-Variable, Arial, sans-serif',
       fontSize: 20,
     },
     h5: {
-      fontFamily: 'Geist Variable, Arial, sans-serif',
+      fontFamily: 'Geist-Variable, Arial, sans-serif',
       fontSize: 16,
     },
     h6: {
-      fontFamily: 'Geist Variable, Arial, sans-serif',
+      fontFamily: 'Geist-Variable, Arial, sans-serif',
       fontSize: 15,
     },
   },
